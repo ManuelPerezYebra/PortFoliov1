8a69be81464d9e142e9c70aa212d0fc2495a16ec
Manuel <manuelperezyebra@gmail.com>
2024-07-22 21:32:52 +0200

    Update mainContainer.styles.js

--- src/components/MainContainer/mainContainer.styles.js
+++ src/components/MainContainer/mainContainer.styles.js
@@ -84,6 +84,7 @@ const ProfileImageContainerMobile = styled.div`
 			width: 150px;
 			height: 150px;
 			border-radius: 20%;
+			box-shadow: 0px 0px 24px 8px rgba(255, 255, 255, 0.12);
 		}
 	}
 `;
